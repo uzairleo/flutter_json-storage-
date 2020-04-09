5586aa9b3ed257f29291b1c743848baf8969097a
Uzair Leo <uzair.jan336@gmail.com>
2020-04-09 19:40:24 +0500

    add some practice work

--- lib/JsonDemo.dart
+++ lib/JsonDemo.dart
@@ -15,25 +15,26 @@ class _JsonDemoState extends State<JsonDemo> {
         centerTitle: true,
       ),
       body: Container(
-        child:Center(
+        child:  Center(
           child:FutureBuilder(
             future: DefaultAssetBundle
             .of(context)
             .loadString('load_json/person.json'),
             builder:(context,snapshot){
-              //decode from json
+              //decode from json(decode means deserialized or translating data structure into a string )
               var myData=json.decode(snapshot.data.toString());
               return ListView.builder(
-                itemCount: data.length,
+                itemCount: (myData==null)?0:myData.length,
                 itemBuilder: (BuildContext context,index){
                   return Card(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text("name: "+myData[index]['name']),
                           Text("Gender: "+myData[index]['gender']),
                           Text("SkinColor: "+myData[index]['skin-color']),
                           Text("Height: "+myData[index]['height']),
+                          SizedBox(height:10.0)
                         ],
                       ),
                   );
